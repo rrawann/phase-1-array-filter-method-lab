7646a0b8f890f75dd0592b3e9615a7269d9784a5
Rawan <rawnq.sb@gmail.com>
2022-12-27 22:42:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+
+function findMatching(drivers, name ){
+    return drivers.filter(item => item.toLowerCase() === name.toLowerCase())
+}
+function fuzzyMatch(drivers, name){
+    return drivers.filter(item => item.startsWith(name) )
+}
+
+
+
+function matchName(drivers,name){
+    {
+        return drivers.filter(item => item.name === name)
+    }
+} 
